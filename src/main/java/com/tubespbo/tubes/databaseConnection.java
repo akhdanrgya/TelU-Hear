03afe4dc60d556495191b76b3dc4726f8d33459e
akhdanrgya <akhdan.anargya@gmail.com>
2024-12-07 23:39:13 +0700

    Update import mysql connector

---
+++ src/main/java/com/tubespbo/tubes/databaseConnection.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tubespbo.tubes;
+
+/**
+ *
+ * @author akhda
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class databaseConnection {
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.tubespbo.tubes {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
     opens com.tubespbo.tubes to javafx.fxml;
     exports com.tubespbo.tubes;
 }
