5d89cf3cc77bea8c08588d7826ec8de7d5b38d27
akhdanrgya <akhdan.anargya@gmail.com>
2024-12-08 01:13:11 +0700

    Update connection to mysql in primary page

--- src/main/java/com/tubespbo/tubes/PrimaryController.java
+++ src/main/java/com/tubespbo/tubes/PrimaryController.java
@@ -5,8 +5,23 @@ import javafx.fxml.FXML;
 
 public class PrimaryController {
 
+    
+    private void cekdb(){
+                try {
+            if (databaseConnection.getConnection() != null) {
+                System.out.println("Berhasil terkoneksi ke database!");
+            } else {
+                System.out.println("Gagal terkoneksi ke database.");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+    
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+        cekdb();
     }
+    
 }
--- src/main/java/com/tubespbo/tubes/databaseConnection.java
+++ src/main/java/com/tubespbo/tubes/databaseConnection.java
@@ -14,5 +14,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class databaseConnection {
-    
+    private static final String URL = "jdbc:mysql://localhost:3306/telu-hear";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Koneksi berhasil ke database!");
+        } catch (SQLException e) {
+            System.err.println("Koneksi gagal: " + e.getMessage());
+        }
+        return connection;
+    }
 }
